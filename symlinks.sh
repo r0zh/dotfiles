2bc8e0cca45549a419c0d4e8eb5d1ef84a5e510c
r0zh <r0zh017@proton.me>
2024-06-28 11:09:17 +0200

    set up hyprpaper

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,4 @@
 ln -sfn ~/dotfiles/fonts/ /usr/local/share/fonts &&
 ln -sfn ~/dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf &&
 ln -sfn ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sfn ~/dotfiles/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
