f62a7f620c05fb8e4d4eef56b03889623e614187
r0zh <r0zh017@proton.me>
2025-02-18 20:36:33 +0100

    trigger media store rescan and refactor

--- scripts/push_file_to_android.sh
+++ scripts/push_file_to_android.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+path="/sdcard/Download/adbpush/"
+
 # Check if an argument was provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <file>"
@@ -13,11 +15,14 @@ if [ ! -f "$1" ]; then
 fi
 
 # Execute the adb command to push the file
-adb push "$1" /sdcard/Download
+adb push "$1" "$path"
+
+# Triger media store rescan
+adb shell content call --method scan_volume --uri content://media --arg external_primary
 
 # Check if the adb command was successful
 if [ $? -eq 0 ]; then
-  echo "File '$1' successfully pushed to /sdcard/Download"
+  echo "File '$1' successfully pushed to $path"
 else
   echo "Error: Failed to push the file '$1'. Ensure the device is connected and adb is properly configured."
   exit 1
